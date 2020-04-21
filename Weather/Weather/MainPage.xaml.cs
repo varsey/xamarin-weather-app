dde61ca8f2617d60164665d79fdd34da0c979a9c
varsey <eugeny.varseev@gmail.com>
2020-04-21 10:34:01 +0300

    enriched class Condition

--- Weather/Weather/MainPage.xaml.cs
+++ Weather/Weather/MainPage.xaml.cs
@@ -26,7 +26,12 @@ namespace Weather
             public Condition condition { get; set; }
             public class Condition
             {
-                public string text { get; set; }
+                [JsonProperty("text")]
+                public string Text { get; set; }
+                [JsonProperty("icon")]
+                public string Icon { get; set; }
+                [JsonProperty("code")]
+                public int Code { get; set; }
             }
         }
 
